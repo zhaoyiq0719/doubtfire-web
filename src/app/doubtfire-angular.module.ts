c11f2944feda5dbc381b81772e83065f19fc1e7f
zhaoyiq0719 <83054303+zhaoyiq0719@users.noreply.github.com>
2022-04-06 23:53:31 +0800

    fix(test):migrate notification component

--- src/app/doubtfire-angular.module.ts
+++ src/app/doubtfire-angular.module.ts
@@ -165,12 +165,13 @@ import { HeaderComponent } from './common/header/header.component';
 import { UnitDropdownComponent } from './common/header/unit-dropdown/unit-dropdown.component';
 import { TaskDropdownComponent } from './common/header/task-dropdown/task-dropdown.component';
 import { SplashScreenComponent } from './home/splash-screen/splash-screen.component';
-
+import { notificationComponent } from './admin/modals/user-notification-settings-modal/notification.component';
 @NgModule({
   // Components we declare
   declarations: [
     AboutDoubtfireModalContent,
     TaskCommentComposerComponent,
+    notificationComponent,
     AudioCommentRecorderComponent,
     MicrophoneTesterComponent,
     DiscussionPromptComposerComponent,
